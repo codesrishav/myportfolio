3d3f4f8b92f0f92643c69d23437336a8547fa439
Rishav <kumarrishav899@gmail.com>
2025-08-15 14:22:15 +0530

    Updated portfolio favicon in index.html

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
   return (
     <footer className="bg-gray-900 text-white py-6 text-center">
       <div className="mb-2 flex justify-center space-x-4">
-        <a href="https://github.com/Bhargav9334" target="_blank" rel="noreferrer" className="hover:text-orange-400">
+        <a href="https://github.com/codesrishav" target="_blank" rel="noreferrer" className="hover:text-orange-400">
           <FaGithub className="text-xl" />
         </a>
-        <a href="https://www.linkedin.com/in/bhargav-sinha-7274132b1" target="_blank" rel="noreferrer" className="hover:text-orange-400">
+        <a href="https://www.linkedin.com/in/rishavkumar5793/" target="_blank" rel="noreferrer" className="hover:text-orange-400">
           <FaLinkedin className="text-xl" />
         </a>
-        <a href="https://twitter.com/yourusername" target="_blank" rel="noreferrer" className="hover:text-orange-400">
+        <a href="https://x.com/RishavKuma79182" target="_blank" rel="noreferrer" className="hover:text-orange-400">
           <FaTwitter className="text-xl" />
         </a>
       </div>
